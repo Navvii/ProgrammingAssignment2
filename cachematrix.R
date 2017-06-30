9fe6b97bda462c35f1e8ac573f0cd05b24fae55c
Navvii <audrey.weaver00@gmail.com>
2017-06-29 21:06:43 -0400

    Caching the Inverse of a Matrix
    Assignment2 -  Caching the Inverse of a Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix puts the matrix in a list
+## then adds a second value in the list as a 
+## placeholder for the inverse
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  SpecialMO <- list(x, NULL)
+  return(SpecialMO)
+  
 }
 
-
-## Write a short comment describing this function
+## cacheSolve looks to see if x[[2]] is NULL and if so, 
+## uses the 'solve' function to calcuate the inverse of x[[1]],
+## store it in x[[2]] (replacing the NULL), and then returns x[[2]]
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
+  # x[[1]] is the matrix
+  # x[[2]] is either NULL or the inverted x[[1]]
+  
+  if(is.null(x[[2]])) {
+    x[[2]] <- solve(x[[1]])
+    
+  }  
+  
+  return(x[[2]])
 }
+
+
